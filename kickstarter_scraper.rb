4d1e53730cf18dc1fe87563decd9bb65016fb416
David Stewart <davidstewart@gmail.com>
2019-04-18 03:14:50 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,7 +6,13 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  projects = {}
+  
+  
+  
+  
+  
+  
 end
 
  title = kickstarter.css("h2.bbcard_name strong a").text
