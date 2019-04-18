888360f3aaf258e0ba3c09ab853dc637a54e2691
David Stewart <davidstewart@gmail.com>
2019-04-18 03:15:17 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,14 +8,20 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   projects = {}
   
-  
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+    binding.pry
+  end
+ 
+  # return the projects hash
+  projects
   
   
   
   
 end
 
- title = kickstarter.css("h2.bbcard_name strong a").text
- image_link = kickstarter.css("div.project-thumbnail a img").attribute("src").value
+# title = kickstarter.css("h2.bbcard_name strong a").text
+# image_link = kickstarter.css("div.project-thumbnail a img").attribute("src").value
  
- description = kickstarter.css("p.bbcard_blurb").text
+# description = kickstarter.css("p.bbcard_blurb").text
